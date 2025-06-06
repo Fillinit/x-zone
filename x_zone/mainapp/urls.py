be9c929e4f795e27b01ef84398a6ff93aca26b69
fillinit <yusuf998915410824@gmail.com>
2025-06-06 08:24:17 +0300

    Настроил форму

--- x_zone/mainapp/urls.py
+++ x_zone/mainapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('application/', views.application, name='application'),
+    path('feedback/footer', views.feedback_footer, name='feedback_footer'),
+    path('feedback/application', views.feedback_application, name='feedback_application'),
 ]
--- x_zone/mainapp/views.py
+++ x_zone/mainapp/views.py
@@ -1,7 +1,12 @@
 from .models import *
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+
+
+
 
 
 
@@ -13,5 +18,24 @@ def application(request):
     template = "mainapp/application.html"
     return render(request, template)
 
-    
+def feedback_footer(request):
+    if request.method == 'POST':
+        name_value = request.POST.get('name')
+        email_value = request.POST.get('email')
+        message_value = request.POST.get('message')
+        print("name_value", name_value)
+        messages.success(request, "Спасибо! Ваше сообщение отправлено.")
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+    else:
+        return render(request, 'mainapp/application.html')
 
+
+def feedback_application(request):
+    if request.method == 'POST':
+        name_value = request.POST.get('name')
+        email_value = request.POST.get('email')
+        message_value = request.POST.get('message')
+        print("name_value", name_value)
+        return redirect('mainapp:index') 
+    else:
+        return render(request, 'mainapp/application.html')
